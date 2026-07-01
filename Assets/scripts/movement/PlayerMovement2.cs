e13f0709c79076bf04e5ff388e9b691726c80260
nban780 <nban780@gmail.com>
2026-07-02 01:42:08 +1000

    fixed some bugs
    replaced boxes and fixed shooting

--- Assets/scripts/movement/PlayerMovement2.cs
+++ Assets/scripts/movement/PlayerMovement2.cs
@@ -12,6 +12,7 @@ public class PlayerMovement2 : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        rb.freezeRotation = true; // страховка: физические удары не должны вращать/разворачивать игрока
         animator = GetComponent<Animator>();
         sr = GetComponent<SpriteRenderer>();
         sr.flipX = true; // по умолчанию смотрит вправо
